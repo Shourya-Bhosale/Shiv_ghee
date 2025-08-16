0c9cd64222ce03ff7e7108b6ccabf3861a7b2234
Shourya-Bhosale <bhosale.shourya255@gmail.com>
2025-08-16 13:24:59 +0530

    Clean reset with proper .gitignore

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    search_fields = ('name', 'phone', 'product__name')
-#    list_filter = ('created_at', 'product')
